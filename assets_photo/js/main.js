cebc3345b0470bd6b9f7a6dc68ae54c1b13e0a02
ankitjain-27 <ankjain@ethz.ch>
2017-04-27 17:03:12 +0200

    included sidebar in photo.html

--- assets_photo/js/main.js
+++ assets_photo/js/main.js
@@ -56,6 +56,21 @@
 					});
 
 			}
+			
+				// Menu.
+			$('#menu')
+				.append('<a href="#menu" class="close"></a>')
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'right',
+					target: $body,
+					visibleClass: 'is-menu-visible'
+				});
 
 		// Scroll back to top.
 			$window.scrollTop(0);
